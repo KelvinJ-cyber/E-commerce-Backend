b2bfa435325b3eb4b9b4b3949028a29d20ff5fe1
Kelvin Justine <justineikechi6@gmail.com>
2025-08-12 00:43:08 +0100

    First Commit

---
+++ src/main/java/com/kelvin/e_com_backend/E_ComBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.e_com_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E_ComBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E_ComBackendApplication.class, args);
+	}
+
+}
