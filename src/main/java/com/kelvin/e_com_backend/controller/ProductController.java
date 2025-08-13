ef89e13204b2695683443006f932bfa693f87881
Kelvin Justine <justineikechi6@gmail.com>
2025-08-13 15:31:13 +0100

    Completed Fetching images

--- src/main/java/com/kelvin/e_com_backend/controller/ProductController.java
+++ src/main/java/com/kelvin/e_com_backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.kelvin.e_com_backend.controller;
 import com.kelvin.e_com_backend.model.Product;
 import com.kelvin.e_com_backend.service.ProductService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,5 +50,15 @@ public class ProductController {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/products/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+         byte[] imageFile = product.getImageData();
+
+         return ResponseEntity.ok()
+                 .contentType(MediaType.valueOf(product.getImageType()))
+                 .body(imageFile);
     }
 }
