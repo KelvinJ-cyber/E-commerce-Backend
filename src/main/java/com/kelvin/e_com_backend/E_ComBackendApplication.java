947861b3fadd04170c82c370f52c0b2097faa361
Kelvin Justine <justineikechi6@gmail.com>
2025-08-14 16:28:48 +0100

    Done !

--- src/main/java/com/kelvin/e_com_backend/E_ComBackendApplication.java
+++ src/main/java/com/kelvin/e_com_backend/E_ComBackendApplication.java
@@ -3,6 +3,8 @@ package com.kelvin.e_com_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// !  Done with the MVC of the project
+
 @SpringBootApplication
 public class E_ComBackendApplication {
 
